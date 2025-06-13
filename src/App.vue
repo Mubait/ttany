242a936ee7fb3e1eb493580479cd1884a092e377
Mubait <kuz2001min@mail.ru>
2025-06-14 02:44:17 +0700

    feat/вёрстка хэдера

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <HeaderMainPg/>
+  </div>
+</template>
+
+<script>
+import HeaderMainPg from './components/HeaderMainPg.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderMainPg
+  }
+}
+</script>
---
+++ src/components/HeaderMainPg.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <div class="frame-1657">
+            <div class="frame-1655">
+                <div class="material-symbols-image">
+                    <img class="vector" src="/common/logo/logo.svg" alt="logo">
+                </div>
+                <p class="logo-txt">Логотип</p>
+            </div>
+            <div class="frame-1654"> 
+                <div class="catalog"> Каталог </div> 
+            </div>
+        </div>
+
+        <div class="search-wrapper">
+            <div class="frame-1167">
+                <img class="search-frame" src="/common/other/search.svg">
+                <input class="search-input" type="text" placeholder="Поиск по ? товаров"/>
+            </div>
+        </div>
+
+        <div class="frame-1656">
+            <p class="information-about-company"> Информация о компании </p>
+            <p class="contacts"> Контакты </p>
+            <p class="useful-links"> Полезные ссылки </p>
+        </div>
+
+        <!-- <img class="autocomplete" src="/public/mobile/autocomplete.svg" alt="logo">
+        <div class="product-search-wrap-mobile">
+            <input class="product-search" type="text" placeholder="Поиск по ? товаров"/>
+            <button class="search-btn"> Найти </button>
+        </div>
+
+        <p class="about-of-company">Информация о компании</p>
+        <p class="contacts">Контакты</p>
+        <p class="useful-links">Полезные ссылки</p> -->
+    </header>
+</template>
