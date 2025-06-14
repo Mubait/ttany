3bf19e41faa0d363ecea40dc31bf659d9ed319c8
Mubait <kuz2001min@mail.ru>
2025-06-14 16:16:34 +0700

    fix/исправлена неполадка, в результате которой элементы выходили за пределы экрана правой границы

--- src/components/HeaderMain.vue
+++ src/components/HeaderMain.vue
@@ -1,5 +1,6 @@
 <template>
     <header>
+        <!-- 1920, 1440 -->
         <div class="frame-1657">
             <div class="frame-1655">
                 <div class="material-symbols-image">
@@ -14,7 +15,7 @@
 
         <div class="search-wrapper">
             <div class="frame-1167">
-                <img class="search-frame" src="/common/other/search.svg">
+                <img class="search-frame" src="/common/other/search.svg" alt="search">
                 <input class="search-input" type="text" placeholder="Поиск по ? товаров"/>
             </div>
         </div>
@@ -24,5 +25,27 @@
             <p class="contacts"> Контакты </p>
             <p class="useful-links"> Полезные ссылки </p>
         </div>
+        <!--  -->
+
+        <!-- 375 -->
+         <div class="frame-1081">
+            <div class="search-form">
+                <div class="input-form-wrapper">
+                    <div class="frame-1140">
+                        <img class="arrow-back" src="/mobile/arrow-back.svg" alt="arrow-back">
+                        <input class="input-form" type="text" placeholder="Запрос"/>
+                        <img class="cross" src="/mobile/cross.svg" alt="cross">
+                        <div class="search-btn"> <p class="search-txt"> Найти </p> </div>
+                    </div>
+                    <div class="frame-1141">
+
+                    </div>
+                </div>
+            </div>
+         </div>
+         <!-- <div class="frame-1094">
+
+         </div> -->
+
     </header>
 </template>
